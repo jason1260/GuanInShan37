fc18ca79f13e1093f44b6c112b8719128487a781
ShuChenLin <ch25585.leader.tw@gmail.com>
2023-06-03 23:59:27 +0800

    nothing

---
+++ guaninshan/assets/Script/game.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, director, Node, Physics2DManifoldType } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('game')
+export class game extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
