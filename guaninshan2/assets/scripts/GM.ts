0bf13be474805a86c392bab7565a2ecee2ae945d
owen920831 <owen920831@gmail.com>
2023-06-04 17:21:49 +0800

    collider

--- guaninshan2/assets/scripts/GM.ts
+++ guaninshan2/assets/scripts/GM.ts
@@ -10,21 +10,49 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
+    @property(cc.TiledMap)
+    tiledMap: cc.TiledMap = null;
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad() {
         cc.director.getPhysicsManager().enabled = true;
         cc.director.getCollisionManager().enabled = true;
+        // cc.director.getPhysicsManager().debugDrawFlags = 1;
     }
 
-    start() {
+    start () {
+        this.drawColliderboxes();
     }
 
-    // update (dt) {}
+    drawColliderboxes (){
+        let tiledSize = this.tiledMap.getTileSize();
+        let layers = ['blocks'];
+        let tag = 15;
+        for (var layerName of layers) {
+            let layer = this.tiledMap.getLayer(layerName);
+            cc.log(layerName, layer);
+            let layerSize = layer.getLayerSize();
+
+            for (let i = 0; i < layerSize.width; i++) {
+                for (let j = 0; j < layerSize.height; j++) {
+                    let tiled = layer.getTiledTileAt(i, j, true);
+                    if (tiled.gid != 0) {
+                        tiled.node.group = 'wall';
+
+                        let body = tiled.node.addComponent(cc.RigidBody);
+                        body.type = cc.RigidBodyType.Static;
+                        let collider = tiled.node.addComponent(cc.PhysicsBoxCollider);
+                        collider.offset = cc.v2(tiledSize.width/2 - 320, tiledSize.height/2 -200);
+                        collider.size = tiledSize;
+                        collider.tag = tag;
+                        collider.friction = 0.2;
+                        collider.apply();
+                    }
+                }
+            }
+            tag++;
+        }
+    }
+
 }
