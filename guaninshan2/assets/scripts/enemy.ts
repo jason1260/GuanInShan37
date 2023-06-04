1a907c475f330d207cb840dff69b7d7fd2cc55bd
ShuChenLin <ch25585.leader.tw@gmail.com>
2023-06-04 17:06:08 +0800

    respawn

--- guaninshan2/assets/scripts/enemy.ts
+++ guaninshan2/assets/scripts/enemy.ts
@@ -10,31 +10,55 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class Enemy extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+    @property(cc.Node)
+    map: cc.Node = null;
 
     @property
     text: string = 'hello';
 
     private life: number;
     private isdead: boolean;
+    private mapSize: cc.Size;
+    private tileSize: cc.Size;
+    private tilemap: cc.TiledMap;
 
     onLoad() {
         this.life = 100;
         this.isdead = false;
+        this.tilemap = this.map.getComponent(cc.TiledMap);
+        this.mapSize = this.tilemap.getMapSize();
+        this.tileSize = this.tilemap.getTileSize();
     }
 
     update(dt) {
         if (this.life <= 0) {
             this.isdead = true;
-            this.life = 100;
+            this.node.active = false;
+            this.scheduleOnce(this.respawnTarget.bind(this), 1);
         }
+    }
+
+    respawnTarget() {
+        this.node.active = true;
+        this.life = 100;
+        this.node.setPosition(this.getRandomPosition());
+    }
+
+    getRandomPosition() {
+        const minX = -this.mapSize.width * this.tileSize.width / 2;
+        const minY = -this.mapSize.height * this.tileSize.height / 2;
+        const maxX = this.mapSize.width * this.tileSize.width / 2;
+        const maxY = this.mapSize.height * this.tileSize.height / 2;
+
+        const x = Math.random() * (maxX - minX) + minX;
+        const y = Math.random() * (maxY - minY) + minY;
+        return new cc.Vec2(x, y);
     }
 
     onCollisionEnter(other, self) {
         console.log("hello knife touch");
         if (other.node.group == 'knife') {
-            console.log("hurts");
+            console.log("hurts ", this.life);
             this.life -= 10;
         }
     }
--- guaninshan2/assets/scripts/hands.ts
+++ guaninshan2/assets/scripts/hands.ts
@@ -5,7 +5,7 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class NewClass extends cc.Component {
@@ -20,87 +20,87 @@ export default class NewClass extends cc.Component {
     @property(cc.Prefab)
     bulletPrefab: cc.Prefab
 
-    
-    public rightAngle:number =-45;
-    public leftAngle:number =45;
-    public rightRadius:number =25;
-    public leftRadius:number =25;
-    public playerTs = null;
-    public mousePt:cc.Vec2 = cc.v2(0,0);
-    public bulletVelocity:number = 1000;
 
-    public timer:number = 0;
-    public attacking:boolean = false;
+    public rightAngle: number = -45;
+    public leftAngle: number = 45;
+    public rightRadius: number = 25;
+    public leftRadius: number = 25;
+    public playerTs = null;
+    public mousePt: cc.Vec2 = cc.v2(0, 0);
+    public bulletVelocity: number = 1000;
+
+    public timer: number = 0;
+    public attacking: boolean = false;
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {
+    onLoad() {
         cc.Canvas.instance.node.on('mousedown', this.attack, this);
         cc.Canvas.instance.node.on('mousedown', this.onMouseMove, this);
         cc.Canvas.instance.node.on('mousemove', this.onMouseMove, this);
         this.playerTs = this.node.getComponent('player');
     }
 
-    start () {
-       
+    start() {
+
     }
 
-    update (dt) {
-        if(this.playerTs.Handstate == 'changing')
+    update(dt) {
+        if (this.playerTs.Handstate == 'changing')
             this.changingAni()
-        if(this.playerTs.Handstate != 'changing' && !this.attacking)
+        if (this.playerTs.Handstate != 'changing' && !this.attacking)
             this.idleAni()
         this.HandPos();
         /* console.log(this.playerTs.Handstate) */
     }
-    attack(){
-        if(this.playerTs.Handstate == 'changing' || this.attacking) return;
+    attack() {
+        if (this.playerTs.Handstate == 'changing' || this.attacking) return;
         this.attacking = true
         switch (this.playerTs.Handstate) {
             case 'gun':
                 this.shoot()
-                this.scheduleOnce(()=>{
+                this.scheduleOnce(() => {
                     this.attacking = false
-                },0.2)
+                }, 0.2)
                 break;
             case 'knife':
                 this.leftAngle = 15;
-                this.scheduleOnce(()=>{
+                this.scheduleOnce(() => {
                     this.leftAngle = 45;
                     this.attacking = false
-                },0.2)
+                }, 0.2)
                 break;
             default:
                 break;
         }
-        
-  
+
+
     }
-    idleAni(){
-        
+    idleAni() {
+
         switch (this.playerTs.Handstate) {
             case 'gun':
-                this.leftAngle = 15   
+                this.leftAngle = 15
                 this.rightAngle = -this.leftAngle;
                 break;
             case 'knife':
-                this.leftAngle = 45   
+                this.leftAngle = 45
                 this.rightAngle = -this.leftAngle;
                 break;
             default:
                 break;
         }
     }
-    changingAni(){
-        this.timer+=1;
-        if(this.timer%10 == 0){
-            if(this.leftAngle != 10)
+    changingAni() {
+        this.timer += 1;
+        if (this.timer % 10 == 0) {
+            if (this.leftAngle != 10)
                 this.leftAngle = 10
             else
                 this.leftAngle = 20
             this.rightAngle = -this.leftAngle;
         }
     }
-    HandPos(){
+    HandPos() {
         let angleInRadians = cc.misc.degreesToRadians(this.leftAngle);
         let xOffset = this.leftRadius * Math.cos(angleInRadians);
         let yOffset = this.leftRadius * Math.sin(angleInRadians);
@@ -121,23 +121,23 @@ export default class NewClass extends cc.Component {
         const dest = this.getRandomInCircle_polar_better(this.playerTs.shootRadius);
 
         // 创建射线的起始点和终点
-        
+
         const startPos = this.node.getPosition();
         // cc.log(startPos);
         const direction = dest.sub(startPos).normalize();
         const endPos = startPos.add(direction.mul(2000));
 
         // 创建射线的绘制节点
-        const bullet = cc.instantiate(this.bulletPrefab);  
+        const bullet = cc.instantiate(this.bulletPrefab);
         const parentNode = this.node.parent;
-        const nodeIndex = parentNode.children.indexOf(this.node);        
-        parentNode.insertChild(bullet, nodeIndex-1);
-        bullet.setPosition(new cc.Vec2(this.node.position.x,this.node.position.y));
+        const nodeIndex = parentNode.children.indexOf(this.node);
+        parentNode.insertChild(bullet, nodeIndex - 1);
+        bullet.setPosition(new cc.Vec2(this.node.position.x, this.node.position.y));
         bullet.getComponent(cc.Collider).enabled = false;
         bullet.getComponent(cc.RigidBody).linearVelocity = direction.mul(this.bulletVelocity);
         console.log(direction)
     }
-    getRandomInCircle_polar_better(radius){
+    getRandomInCircle_polar_better(radius) {
         let length = Math.sqrt(Math.random()) * radius;
         let angle = Math.random() * 2 * Math.PI;
         // 然後將極座標轉成xy座標的Point
@@ -155,18 +155,18 @@ export default class NewClass extends cc.Component {
         const x = length * Math.cos(angle);
         const y = length * Math.sin(angle);
         return cc.v2(x, y);
-    }  
+    }
     onMouseMove(event: cc.Event.EventMouse) {
         // 更新空心圆圈的位置与鼠标位置一致
-        
+
 
         const mousePos = event.getLocation();
 
-    // 将鼠标坐标转换为玩家节点的本地坐标系
+        // 将鼠标坐标转换为玩家节点的本地坐标系
         const playerLocalPos = this.node.parent.convertToNodeSpaceAR(mousePos);
 
         // 在玩家节点的本地坐标系中操作
- 
+
         this.mousePt = playerLocalPos
     }
 }
