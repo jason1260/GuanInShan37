db32e0177339c09ff072605b7bfb255114c46f40
YuYi <why33551@gmail.com>
2023-06-06 16:12:48 +0800

    knife animation

--- guaninshan2/assets/scripts/hands.ts
+++ guaninshan2/assets/scripts/hands.ts
@@ -59,30 +59,61 @@ export default class NewClass extends cc.Component {
         /* console.log(this.playerTs.Handstate) */
     }
     attack() {
-        if (this.playerTs.Handstate == 'changing' || this.attacking) return;
-        this.attacking = true
+        if (this.playerTs.Handstate === 'changing' || this.attacking) return;
+        this.attacking = true;
+      
         switch (this.playerTs.Handstate) {
-            case 'gun':
-                this.shoot()
-                this.scheduleOnce(() => {
-                    this.attacking = false
-                }, 0.2)
-                break;
-            case 'knife':
-                this.leftAngle = 15;
-                knife_valid = true;
-                this.scheduleOnce(() => {
-                    this.leftAngle = 45;
-                    this.attacking = false
-                    knife_valid = false;
-                }, 0.2)
-                break;
-            default:
-                break;
+          case 'gun':
+            this.shoot();
+            this.scheduleOnce(() => {
+              this.attacking = false;
+            }, 0.2);
+            break;
+      
+          case 'knife':
+            this.leftAngle = 0;
+            knife_valid = true;
+      
+            // 逐渐改变角度的函数
+            const rotateAngle = (targetAngle: number, duration: number) => {
+              const startAngle = this.leftAngle;
+              let elapsedTime = 0;
+              const interval = 0.02; // 每帧间隔时间，可根据需要调整
+      
+              const update = () => {
+                elapsedTime += interval;
+                const t = Math.min(elapsedTime / duration, 1); // 插值参数
+      
+                // 根据插值参数 t 计算当前角度
+                this.leftAngle = cc.misc.lerp(startAngle, targetAngle, t);
+      
+                if (t >= 1) {
+                  // 动画结束后的操作
+                  this.leftAngle = 45;
+                  this.attacking = false;
+                  knife_valid = false;
+                } else {
+                  // 继续更新角度
+                  setTimeout(update, interval * 1000);
+                }
+              };
+      
+              update();
+            };
+      
+            rotateAngle(45, 0.2); // 调用函数开始逐渐改变角度
+      
+            break;
+      
+          default:
+            break;
         }
-
-
-    }
+      }
+      //在这个示例中，我添加了一个名为 rotateAngle 的函数，该函数用于逐渐改变角度。它接受目标角度和持续时间作为参数，并使用递归调用来实现逐帧更新角度直到达到目标角度为止。可以根据需要调整每帧的间隔时间 interval，以控制动画的流畅度。
+      
+      
+      
+      
     idleAni() {
 
         switch (this.playerTs.Handstate) {
