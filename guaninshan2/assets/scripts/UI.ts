702d58d80e0f034d8cc25dba6b13a3caf43ee865
110062207 <jason20031107@gmail.com>
2023-06-15 17:31:20 +0800

    UI v1

--- guaninshan2/assets/scripts/UI.ts
+++ guaninshan2/assets/scripts/UI.ts
@@ -7,7 +7,13 @@ export default class UI extends cc.Component {
     hpBar: cc.ProgressBar = null;
 
     @property(cc.Node)
-    hpBarColor:cc.Node = null;
+    hpBarColor: cc.Node = null;
+
+    @property(cc.Label)
+    bulletLabel: cc.Label = null;
+
+    @property(cc.Label)
+    scoreLabel: cc.Label = null;
 
     private playerTs = null;
 
@@ -15,6 +21,10 @@ export default class UI extends cc.Component {
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
         this.playerTs = cc.find('Canvas/Main Camera/player').getComponent('player');
+        this.bulletLabel.string = this.playerTs.bulletNum;
+        this.hpBar.progress = this.playerTs.HP / 100;
+        this.hpBarColor.color = cc.color((1-Math.pow(this.hpBar.progress, 6))*255, this.hpBar.progress*255, 0);
+        this.scoreLabel.string = this.playerTs.score;
     }
 
     start () {
@@ -22,19 +32,33 @@ export default class UI extends cc.Component {
     }
 
     update (dt) {
-        this.hpBar.progress = this.playerTs.HP / 100;
-        this.hpBarColor.color = cc.color((1-Math.pow(this.hpBar.progress, 6))*255, this.hpBar.progress*255, 0);
+
     }
 
     onKeyDown (event) {
         if (event.keyCode === cc.macro.KEY.o && this.playerTs.HP > 0) {
             this.playerTs.HP -= 1;
+            this.hpUpdate();
         } else if (event.keyCode === cc.macro.KEY.p && this.playerTs.HP < 100) {
             this.playerTs.HP += 1;
+            this.hpUpdate();
         }
     }
 
     onKeyUp () {
 
+    }
+
+    hpUpdate () {
+        this.hpBar.progress = this.playerTs.HP / 100;
+        this.hpBarColor.color = cc.color((1-Math.pow(this.hpBar.progress, 6))*255, this.hpBar.progress*255, 0);
+    }
+
+    bulletUpdate () {
+        this.bulletLabel.string = this.playerTs.bulletNum;
+    }
+
+    scoreUpdate () {
+        this.scoreLabel.string = this.playerTs.score;
     }
 }
