4e7cf283f1ec02c5a0c0dad6a3c39e522d99eb6a
ShuChenLin <ch25585.leader.tw@gmail.com>
2023-06-16 16:04:22 +0800

    bullet speed

--- guaninshan2/assets/scripts/gameInfo.ts
+++ guaninshan2/assets/scripts/gameInfo.ts
@@ -5,49 +5,49 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 const gameInfo = {
-    weaponWeight : {
-        knife:30,
-        gun:50,
-        rifle:70,
-        sniper:100
+    weaponWeight: {
+        knife: 30,
+        gun: 50,
+        rifle: 70,
+        sniper: 100
     },
-    weaponbulletNum : {
-        knife:1,
-        gun:10,
-        rifle:20,
-        sniper:5
+    weaponbulletNum: {
+        knife: 1,
+        gun: 10,
+        rifle: 20,
+        sniper: 5
     },
-    weaponAttackTime : {
+    weaponAttackTime: {
         knife: 0.02,
-        gun:0.5,
-        rifle:0.2,
-        sniper:1
+        gun: 0.5,
+        rifle: 0.2,
+        sniper: 1
     },
-    rangedWeapon : ['gun','rifle','sniper'],
-    nearWeapon : ['knife'],
-    weaponRadius : {
-        reload:1,
-        changing:1,
-        knife:1,
-        gun:1,
-        rifle:1.5,
-        sniper:0.5
+    rangedWeapon: ['gun', 'rifle', 'sniper'],
+    nearWeapon: ['knife'],
+    weaponRadius: {
+        reload: 1,
+        changing: 1,
+        knife: 1,
+        gun: 1,
+        rifle: 1.5,
+        sniper: 0.5
     },
-    dropsTag2weapon : {
-        0:'rifle',
-        1:'sniper'
+    dropsTag2weapon: {
+        0: 'rifle',
+        1: 'sniper'
     },
     weaponDamage: {
-        knife:15,
-        gun:15,
-        rifle:10,
-        sniper:80
+        knife: 15,
+        gun: 15,
+        rifle: 10,
+        sniper: 80
     },
     bulletVelocity: {
-        knife:1,
-        gun:100,
-        rifle:1000,
-        sniper:1000
+        knife: 1,
+        gun: 1000,
+        rifle: 1000,
+        sniper: 1000
     }
 }
 
